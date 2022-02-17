149050e4458882ad5a01622c35bf6dadbfa5ddf3
ZeBraHack0 <git@github.com>
2022-02-17 13:07:26 +0800

    update prototrans

--- inci_lib/trans/protoTrans.cc
+++ inci_lib/trans/protoTrans.cc
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 
+#include <google/protobuf/map_field.h>
+
 #include "inci_lib/lib/comm.h"
 #include "inci_lib/trans/protoTrans.h"
 #include "inci_lib/trans/quantize.h"
@@ -112,7 +114,7 @@ bool InciStub::IncSend(const google::protobuf::Message &request, google::protobu
         else assert(op==2);
 
         char* data;
-        data = (char*)malloc(sz*sizeof(int)*2);
+        data = (char*)malloc(sz*sizeof(int));
         auto requestArray = requestFieldRefl->GetRepeatedField<int>(requestField, requestFieldFdes);
         memcpy(data, &(requestArray[0]), sz*sizeof(int));
 
@@ -130,7 +132,7 @@ bool InciStub::IncSend(const google::protobuf::Message &request, google::protobu
           if(replyFdes->type()==google::protobuf::FieldDescriptor::TYPE_MESSAGE){
             const google::protobuf::Descriptor* replyFieldDes = replyFdes->message_type();
             printf("inci field type in reply: %s\n", replyFieldDes->name().c_str());
-            if(replyFieldDes->name() == "aggtrArray"){
+            if(replyFieldDes->name() == "aggtrMap"){
               const google::protobuf::Reflection* replyRefl = reply->GetReflection();
               google::protobuf::Message * replyField = replyRefl->MutableMessage(reply, replyFdes);
               const google::protobuf::FieldDescriptor* replyFieldFdes = replyFieldDes->field(0); //data field of aggtrArray
@@ -140,7 +142,53 @@ bool InciStub::IncSend(const google::protobuf::Message &request, google::protobu
               replyArray->Resize(sz, 0);
               memcpy(&((*replyArray)[0]), data, sz*sizeof(int));
               break;
-            } else if(replyFieldDes->name() == "aggtrMap"){
+            }
+          }
+        }
+
+        free(data);
+        sendSucc = true;
+        break;
+      } else if(requestFieldDes->name() == "MonitorMap"){
+        const google::protobuf::Reflection* requestRefl = request.GetReflection();
+        google::protobuf::Message * requestField = requestRefl->MutableMessage(const_cast<google::protobuf::Message*>(&request), requestFdes);
+        const google::protobuf::FieldDescriptor* requestFieldFdes = requestFieldDes->field(0); //data field of aggtrArray
+        assert(requestFieldFdes != nullptr);  // in case of typo or something.
+        const google::protobuf::Reflection* requestFieldRefl = requestField->GetReflection();
+        const google::protobuf::FieldDescriptor* cntForwardFdes = requestFieldDes->field(1); //data field of aggtrArray
+        int cntForward = requestFieldRefl->GetInt32(*requestField, cntForwardFdes);
+        int sz = requestFieldRefl->FieldSize(*requestField, requestFieldFdes);
+        assert(sz>0);
+
+        if(hasInit == false)initService(3,0,2*sz*sizeof(int),1,2000,0);
+        else assert(op==3);
+
+        char* data;
+        data = (char*)malloc(sz*sizeof(int)*2);
+        auto it = requestFieldRefl->MapBegin(requestField, requestFieldFdes);
+        for(int i = 0; i<sz; i++){
+          auto tmp_key = it.GetKey();
+          memcpy(data+2*i*sizeof(int), &tmp_key, sizeof(int));
+          auto tmp_value = it.GetValueRef();
+          memcpy(data+(2*i+1)*sizeof(int), &tmp_value, sizeof(int));
+          it++;
+        }
+
+        comm::MsgArgs *args = createArgs(globalId, 2*sizeof(int)*sz, cntForward, 2000, 0, op);
+        printf("inci send data! %d %d\n", globalId, 2*(int)(sizeof(int)*sz));
+        client->PushPull(data, args);
+        free(args);
+
+        // fill reply
+        const google::protobuf::Descriptor* replyDes = reply->GetDescriptor();
+        int replyFieldNum = replyDes->field_count();
+        for(int i = 0; i<replyFieldNum; i++){
+          const google::protobuf::FieldDescriptor* replyFdes = replyDes->field(i);
+          assert(replyFdes != nullptr);  // in case of typo or something.
+          if(replyFdes->type()==google::protobuf::FieldDescriptor::TYPE_MESSAGE){
+            const google::protobuf::Descriptor* replyFieldDes = replyFdes->message_type();
+            printf("inci field type in reply: %s\n", replyFieldDes->name().c_str());
+            if(replyFieldDes->name() == "MonitorMap"){
               const google::protobuf::Reflection* replyRefl = reply->GetReflection();
               google::protobuf::Message * replyField = replyRefl->MutableMessage(reply, replyFdes);
               const google::protobuf::FieldDescriptor* replyFieldFdes = replyFieldDes->field(0); //data field of aggtrArray
